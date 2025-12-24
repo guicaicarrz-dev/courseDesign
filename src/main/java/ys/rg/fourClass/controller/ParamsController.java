2cd208a9bc537e9398edba5c300703c5ce3b810d
ljq4946 <1743931738@qq.com>
2025-12-24 15:43:57 +0800

    111

---
+++ src/main/java/ys/rg/fourClass/controller/ParamsController.java
@@ -0,0 +1,22 @@
+package ys.rg.fourClass.controller;
+
+
+/**
+ * 系统参数管理
+ */
+public class ParamsController {
+
+    /**
+     * 获取默认系统参数
+     * @return 默认系统参数
+     */
+
+
+    /**
+     * 校验用户输入的系统参数合法性
+     * @param   用户定义参数
+     * @return 是否合法
+     */
+
+
+}
