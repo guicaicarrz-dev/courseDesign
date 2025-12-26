916f5994495011e482e67f6d0f461a6c3c1351eb
ljq4946 <1743931738@qq.com>
2025-12-26 15:52:42 +0800

    txt地址检验 本地文件读取 优化校验地址序列

---
+++ src/main/java/ys/rg/fourClass/service/impl/SpringTxtFileReaderServiceImpl.java
@@ -0,0 +1,52 @@
+package ys.rg.fourClass.service.impl;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import ys.rg.fourClass.service.SpringTxtFileReaderService;
+import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class SpringTxtFileReaderServiceImpl implements SpringTxtFileReaderService {
+    @Resource
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public String readTxtByResourceLoader(String filePath) throws IOException {
+
+        // 获取Resource对象
+        org.springframework.core.io.Resource resource = resourceLoader.getResource("file:" + filePath);
+
+        // 校验文件是否存在
+        if (!resource.exists()) {
+            throw new IOException("文件不存在，路径：" + filePath);
+        }
+
+        // 读取文件
+        String content = getString(filePath, resource);
+        if (!content.isEmpty()) {
+            content = content.substring(0, content.length() - System.lineSeparator().length());
+        }
+        return content;
+    }
+
+    private static String getString(String filePath, org.springframework.core.io.Resource resource) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                contentBuilder.append(line);
+                contentBuilder.append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new IOException("读取文件失败，路径：" + filePath, e);
+        }
+
+        return contentBuilder.toString();
+    }
+
+}
--- src/main/java/ys/rg/fourClass/util/CheckTheFormat.java
+++ src/main/java/ys/rg/fourClass/util/CheckTheFormat.java
@@ -5,5 +5,8 @@ public class CheckTheFormat {
      * 地选字符正则
      */
     public static final String ADDRESS_SEQ_REGEX = "^([0-9A-Fa-f]+H)(\\、|\\，)([0-9A-Fa-f]+H)(?:\\2[0-9A-Fa-f]+H)*$";
-
+    /**
+     * txt文件地选正则
+     */
+    public static final String WINDOWS_TXT_PATH_REGEX = "^[A-Za-z]:\\\\(?:[^\\\\/:*?\"<>|]+[\\\\/])*[^\\\\/:*?\"<>|]+\\.txt$";
 }
